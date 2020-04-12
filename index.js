730198198da816c01719ad7e38bc5964e5821444
Philzace1 <unknownumeh@gmail.com>
2020-04-13 00:37:47 +0100

    something

---
+++ index.js
@@ -0,0 +1,59 @@
+/* eslint-disable linebreak-style */
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const covid19ImpactEstimator = require('./src/estimator');
+
+const app = express();
+
+const t = (tokens, req, res) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  [
+    tokens.method(req, res),
+    '\t\t',
+    tokens.url(req, res),
+    '\t\t',
+    tokens.status(req, res),
+    '\t\t',
+    tokens['response-time'](req, res),
+    'ms'
+  ].join(' ');
+// app.use(morgon('tiny'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(
+  logger(t, {
+    stream: fs.createWriteStream('./logs.log', { flags: 'a' })
+  })
+);
+app.use(logger('dev'));
+app.use(
+  require('xml-express-middleware').xml({
+    transformXmlKeys: 'decamelize',
+    rootXmlKey: 'someElement'
+  })
+);
+
+app.post('/api/v1/on-covid-19/', (req, res) => {
+  const result = covid19ImpactEstimator(req.body);
+  res.send(result);
+});
+app.post('/api/v1/on-covid-19/:format', (req, res) => {
+  const result = covid19ImpactEstimator(req.body);
+  const { format } = req.params;
+  if (format === 'json') {
+    res.header('Content-Type', 'application/json');
+    res.json(result);
+  } else if (format === 'xml') {
+    res.header('Content-Type', 'text/xml');
+    res.xml(result);
+  } else {
+    res.status(404).send('Route not found \n please try json or xml format ');
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => logger('server is on'));
--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,92 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
 
-export default covid19ImpactEstimator;
+// const Data = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 14
+// };
+// 1380614
+
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, periodType, timeToElapse } = data;
+
+  let period = timeToElapse;
+  if (periodType === 'weeks') {
+    period = timeToElapse * 7;
+  } else if (periodType === 'months') {
+    period = timeToElapse * 30;
+  } else {
+    period = timeToElapse;
+  }
+
+  const currentlyInfected = (cases) => ({
+    impact: cases * 10,
+    severe: cases * 50
+  });
+  const estimate = (i) => {
+    const factor = Math.floor(period / 3);
+    return {
+      impact: Math.floor(i.impact * 2 ** factor),
+      severe: Math.floor(i.severe * 2 ** factor)
+    };
+  };
+  const hospitalSpace = (severed) => {
+    const availbleSpace = Math.floor(0.35 * data.totalHospitalBeds);
+    return {
+      impact: availbleSpace - severed.impact,
+      severe: availbleSpace - severed.severe
+    };
+  };
+  const infected = currentlyInfected(reportedCases);
+  const estimation = estimate(infected);
+  const severeCases = {
+    impact: estimation.impact * 0.15,
+    severe: estimation.severe * 0.15
+  };
+  const hospital = hospitalSpace(severeCases);
+  const inflight = (es) => {
+    const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data.region;
+    const { impact, severe } = es;
+    const inflightImpact = (impact * avgDailyIncomeInUSD) / period;
+    const inflightSevere = (severe * avgDailyIncomeInUSD) / period;
+
+    return {
+      impact: Math.floor(inflightImpact * avgDailyIncomePopulation),
+      severe: Math.floor(inflightSevere * avgDailyIncomePopulation)
+    };
+  };
+  const d = inflight(estimation);
+  return {
+    data,
+    impact: {
+      currentlyInfected: infected.impact,
+      infectionsByRequestedTime: estimation.impact,
+      severeCasesByRequestedTime: severeCases.impact,
+      hospitalBedsByRequestedTime: hospital.impact,
+      casesForICUByRequestedTime: 0.05 * estimation.impact,
+      casesForVentilatorsByRequestedTime: 0.02 * estimation.impact,
+      dollarsInFlight: d.impact
+    },
+    severeImpact: {
+      currentlyInfected: infected.severe,
+      infectionsByRequestedTime: estimation.severe,
+      severeCasesByRequestedTime: severeCases.severe,
+      hospitalBedsByRequestedTime: hospital.severe,
+      casesForICUByRequestedTime: 0.05 * estimation.severe,
+      casesForVentilatorsByRequestedTime: 0.02 * estimation.severe,
+      dollarsInFlight: d.severe
+    }
+  };
+};
+// console.log(covid19ImpactEstimator(Data));
+module.exports = covid19ImpactEstimator;
